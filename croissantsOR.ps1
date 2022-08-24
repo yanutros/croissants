ac08a97301ce9e4e0892eafd65234e06ec9ceffb
Yanutros <75123766+yanutros@users.noreply.github.com>
2022-08-24 17:57:27 +0200

    Update croissantsOR.ps1

--- croissantsOR.ps1
+++ croissantsOR.ps1
@@ -1,16 +1,15 @@
 Start-Process "C:\Program Files\Rocket.Chat+\Rocket.Chat.exe" $wshell
-Start-Sleep 5
-
+Start-Sleep 4
 $wshell = New-Object -ComObject wscript.shell;
 $wshell.AppActivate('Cosium.Chat')
 Sleep 1
 $wshell.SendKeys("^(k)")
 Sleep 1
-$wshell.SendKeys("aba")
+$wshell.SendKeys("fpa")
 Sleep 1
 $wshell.SendKeys("{ENTER}")
 Sleep 1
-$wshell.SendKeys(":croissant: ")
+$wshell.SendKeys(":croissant: i wan't des croissants")
 Sleep 2
 $wshell.SendKeys("{ENTER}")
 Sleep 1
